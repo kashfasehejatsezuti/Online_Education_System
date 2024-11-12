1281ded1e9fd6438c787a99745cd8ef4dfcde5c3
kashfasehejatsezuti <you@example.com>
2024-11-12 02:24:42 +0100

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule, JsonPipe, UpperCasePipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'online_education';
+  courseList: any[] = [];
+  http = inject(HttpClient);
+  courseObj: any = {
+    title: '',
+    body: '',
+    userId: 1,
+  };
+  ngOnInit() {
+    this.http
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .subscribe((res: any) => {
+        debugger;
+        this.courseList = res;
+        console.log(res);
+      });
+  }
+
+  onSubmit() {
+    this.http
+      .post('https://jsonplaceholder.typicode.com/posts', this.courseObj)
+      .subscribe(
+        (response) => {
+          alert('Post created successfully!');
+          console.log('Post created successfully!', response);
+          this.courseObj.reset();
+        },
+        (error) => {
+          console.error('Failed to create post', error);
+        }
+      );
+  }
 }
